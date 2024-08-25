abd1c8a3540537c19e135a5268a78fcaad56557e
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2024-08-25 01:09:26 -0300

    Implement free draw canvas

---
+++ components/Canvas.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+import { Stage, Layer, Line } from "react-konva";
+import Toolbar from "./Toolbar";
+
+interface LineType {
+  tool: string;
+  points: number[];
+}
+
+export default function Canvas() {
+  const [tool, setTool] = useState<string>("pen");
+  const [lines, setLines] = useState<LineType[]>([]);
+  const isDrawing = useRef<boolean>(false);
+  const [stageSize, setStageSize] = useState<{ width: number; height: number }>(
+    {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    }
+  );
+
+  const handleMouseDown = (e: any) => {
+    isDrawing.current = true;
+    const pos = e.target.getStage().getPointerPosition();
+    setLines([...lines, { tool, points: [pos.x, pos.y] }]);
+  };
+
+  const handleMouseMove = (e: any) => {
+    // no drawing - skipping
+    if (!isDrawing.current) {
+      return;
+    }
+    const stage = e.target.getStage();
+    const point = stage.getPointerPosition();
+    let lastLine = lines[lines.length - 1];
+    // add point
+    lastLine.points = lastLine.points.concat([point.x, point.y]);
+
+    // replace last
+    lines.splice(lines.length - 1, 1, lastLine);
+    setLines(lines.concat());
+  };
+
+  const handleMouseUp = () => {
+    isDrawing.current = false;
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setStageSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div>
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+        onMouseDown={handleMouseDown}
+        onMousemove={handleMouseMove}
+        onMouseup={handleMouseUp}
+      >
+        <Layer>
+          {lines.map((line, i) => (
+            <Line
+              key={i}
+              points={line.points}
+              stroke="blue"
+              strokeWidth={5}
+              tension={0.5}
+              lineCap="round"
+              lineJoin="round"
+              globalCompositeOperation={
+                line.tool === "eraser" ? "destination-out" : "source-over"
+              }
+            />
+          ))}
+        </Layer>
+      </Stage>
+      <Toolbar selectTool={setTool} resetCanvas={() => setLines([])} />
+    </div>
+  );
+}
