5860778b0ec8f271f503edd78715092872b1600d
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2024-08-31 22:31:01 -0300

    Refactor to use Redux

---
+++ redux/canvasSlice.ts
@@ -0,0 +1,81 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { CanvasObjectType } from "../components/Canvas";
+
+interface CanvasState {
+  canvasObjects: CanvasObjectType[];
+  undoStack: CanvasObjectType[][];
+  redoStack: CanvasObjectType[][];
+  selectedObjectId: string;
+}
+
+const initialState: CanvasState = {
+  canvasObjects: [],
+  undoStack: [],
+  redoStack: [],
+  selectedObjectId: "",
+};
+
+const canvasSlice = createSlice({
+  name: "canvas",
+  initialState,
+  reducers: {
+    addCanvasObject(state, action: PayloadAction<CanvasObjectType>) {
+      state.undoStack.push(state.canvasObjects);
+      state.canvasObjects = [...state.canvasObjects, action.payload];
+      state.redoStack = [];
+    },
+    updateCanvasObject(
+      state,
+      action: PayloadAction<{ id: string; updates: Partial<CanvasObjectType> }>
+    ) {
+      state.undoStack.push(state.canvasObjects);
+      state.canvasObjects = state.canvasObjects.map((object) =>
+        object.id === action.payload.id
+          ? { ...object, ...action.payload.updates }
+          : object
+      );
+      state.redoStack = [];
+    },
+    deleteCanvasObject(state, action: PayloadAction<string>) {
+      state.undoStack.push(state.canvasObjects);
+      state.canvasObjects = state.canvasObjects.filter(
+        (obj) => obj.id !== action.payload
+      );
+      state.selectedObjectId = "";
+      state.redoStack = [];
+    },
+    selectCanvasObject(state, action: PayloadAction<string>) {
+      state.selectedObjectId = action.payload;
+    },
+    resetCanvas(state) {
+      state.undoStack.push(state.canvasObjects);
+      state.canvasObjects = [];
+      state.selectedObjectId = "";
+      state.redoStack = [];
+    },
+    undo(state) {
+      if (state.undoStack.length > 0) {
+        state.redoStack.push(state.canvasObjects);
+        state.canvasObjects = state.undoStack.pop()!;
+      }
+    },
+    redo(state) {
+      if (state.redoStack.length > 0) {
+        state.undoStack.push(state.canvasObjects);
+        state.canvasObjects = state.redoStack.pop()!;
+      }
+    },
+  },
+});
+
+export const {
+  addCanvasObject,
+  updateCanvasObject,
+  deleteCanvasObject,
+  selectCanvasObject,
+  resetCanvas,
+  undo,
+  redo,
+} = canvasSlice.actions;
+
+export default canvasSlice.reducer;
