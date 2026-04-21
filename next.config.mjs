fe0bdb15ba31b30bafeeca48e56cf3c817393662
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2026-04-20 23:27:49 -0300

    fix css for scrollbar

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 
+const isGithubPages =
+  process.env.NODE_ENV === "production" && process.env.GITHUB_PAGES === "true";
+
 const nextConfig = {
   output: "export", // enables static exports
-  basePath: "/konva-whiteboard",
-  assetPrefix: "/konva-whiteboard/",
+  ...(isGithubPages && {
+    basePath: "/konva-whiteboard",
+    assetPrefix: "/konva-whiteboard/",
+  }),
 };
 
 export default nextConfig;
